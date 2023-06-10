51923313eeb168bc5a95402727ee16937827a20d
Vitalii <tomrain78@gmail.com>
2023-06-10 13:11:56 +0300

    adding handle faq

--- src/modules/main.js
+++ src/modules/main.js
@@ -3,7 +3,8 @@ const menuButton = document.querySelector(".header-menu__button"),
   menuLink = document.querySelectorAll(".menu-link"),
   video = document.querySelector("#video"),
   videoButton = document.querySelector(".video-btn"),
-  checkBox = document.querySelectorAll(".checkbox");
+  checkBox = document.querySelectorAll(".checkbox"),
+  faqItem = document.querySelectorAll(".faq-item");
 
 let isPlay = false;
 
@@ -57,7 +58,17 @@ function handleCheckBox({ currentTarget: { checked, name } }) {
   console.log(value);
 }
 
+function handleFaqItem({ currentTarget: target }) {
+  target.classList.toggle(classes.opened);
+  const isOpened = target.classList.contains(classes.opened);
+  const height = target.querySelector("p").clientHeight;
+  const content = target.querySelector(".faq-item__content");
+
+  content.style.height = `${isOpened ? height : 0}px`;
+}
+
 menuButton.addEventListener("click", toggleMenu);
 videoButton.addEventListener("click", handleVideo);
 menuLink.forEach((link) => link.addEventListener("click", scrollToSection));
 checkBox.forEach((box) => box.addEventListener("click", handleCheckBox));
+faqItem.forEach((item) => item.addEventListener("click", handleFaqItem));
