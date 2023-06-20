502e1817d4a78ebe1659f506acbd1fa2499fa662
Vitalii <tomrain78@gmail.com>
2023-06-20 15:26:22 +0300

    adding swiper for editions and adaptive

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -3,7 +3,7 @@ import "swiper/css/bundle";
 Swiper.use([Navigation]);
 
 function slider() {
-  new Swiper(".swiper", {
+  let slider1 = new Swiper(".slider1", {
     loop: true,
     slidesPerView: "auto",
     spaceBetween: 20,
@@ -21,6 +21,34 @@ function slider() {
       },
       1150: {
         slidesPerView: 2,
+      },
+    },
+  });
+
+  let slider2 = new Swiper(".slider2", {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    initialSlide: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+      900: {
+        slidesPerView: 3,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 4,
       },
     },
   });
